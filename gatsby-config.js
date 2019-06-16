4c5848b2992ce19ecb5ad8cc861a308ded66e250
snishimura <sjiro.nishimura@gmail.com>
2019-06-17 01:36:40 +0900

    Fix RSS feed setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,8 +77,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Undefined xxx`,
+        short_name: `Undefined xxx`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
